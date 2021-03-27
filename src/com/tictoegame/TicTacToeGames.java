08c70fe313b2bcc75f6970b741336eca9d082185
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-27 08:59:17 +0530

    welcome TicTacToeGame

---
+++ src/com/tictoegame/TicTacToeGames.java
@@ -0,0 +1,14 @@
+package com.tictoegame;
+
+public class TicTacToeGames {
+	
+
+	public static void main(String[] args) {
+		
+		//char[] board = new char[10];
+		System.out.println("welcome to Tic tac toe game");
+		
+
+	}
+
+}
